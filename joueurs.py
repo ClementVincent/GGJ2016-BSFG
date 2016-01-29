5b99da84545f65e3a1a284c975241ca58cc1aa51
TsiryMAYET <mayet.tsiry@hotmail.com>
2016-01-29 20:53:13 +0100

    création du fichier joueurs.py

---
+++ joueurs.py
@@ -0,0 +1,10 @@
+class Joueurs(object):
+	
+	def __init__(self, nbJoueurs=2,nbVillagois=10,nbGuerrier=5,pa=10,pm=5):
+		self.nbJoueurs=nbJoueurs
+		
+
+
+	# crée un joueur, avec son numero, son nombre de guerrier, son nombre de villageois, 
+
+		
