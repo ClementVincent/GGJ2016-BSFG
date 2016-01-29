23c6f609b55ff1b77dfda85a9562f70c0ddf569b
ClementVincent <freemoute007@laposte.net>
2016-01-29 21:43:32 +0100

    Ajout des methodes prendVillageois et testPasVillageois dans guerrier.py

--- guerrier.py
+++ guerrier.py
@@ -4,6 +4,14 @@ class Guerrier (object) :
 		self.vie = vie
 		self.numJoueur = numJoueur
 		self.numGuerrier = numGuerrier
+		self.villageois = None
 	
 	def perdVie (self,pointDgt) :
 		self.vie -= pointDgt
+	
+	def prendVillageois (self, numVillageois) :
+		self.villageois = numVillageois
+	
+	def testPasVillageois (self) :
+		return self.villageois == None
+
