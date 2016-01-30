50db99534491d1f9307b24cf4e7488f455df4756
ClementVincent <freemoute007@laposte.net>
2016-01-30 02:59:41 +0100

    fze,fei

--- graphique.py
+++ graphique.py
@@ -7,14 +7,14 @@ pygame.init()
 
 
 def afficheLaby (fenetre, labyrinthe) :
-	myfont = pygame.font.SysFont("monospace", 15)
-	label = myfont.render("BLAAAAAAAAAAAAAA", 1, (255,255,0))
-	fenetre.blit(label, (100, 100))
+	myfont = pygame.font.SysFont("monospace",20)
+	FDT = myfont.render("Fin du tour", 1, (255,255,0))
+	fenetre.blit(FDT, (1650, 975))
 	pygame.display.flip()
 
 
 def lancerJeu(labyrinthe) :
-	fenetre = pygame.display.set_mode((640,480))
+	fenetre = pygame.display.set_mode((1800,1024))
 	afficheLaby(fenetre,labyrinthe)
 	continuer = 1
 	changement = 1
@@ -27,46 +27,36 @@ def lancerJeu(labyrinthe) :
 				changement = 0
 				print("ok")
 			if event.type == KEYDOWN and event.key == K_UP :
-				labyrinthe.jouerTour("M",labyrinthe.getCoordG()[0] + 1, labyrinthe.getCoordG()[1])
+				if labyrinthe.plateauq.getVal(labyrinthe.getCoordG()[1],labyrinthe.getCoordG()[0]).passageNord(labyrinthe.plateau.getVal(labyrinthe.getCoordG()[1]+1,labyrinthe.getCoordG()[0])) :
+					labyrinthe.jouerTour("M",labyrinthe.getCoordG()[0] + 1, labyrinthe.getCoordG()[1])
+				else : 
+					message = "La position n'est pas acessible"
 			elif event.type == KEYDOWN and event.key == K_DOWN :
-				labyrinthe.jouerTour("M",labyrinthe.getCoordG()[0]-1, labyrinthe.getCoordG()[1])
+				if labyrinthe.plateau.getVal(labyrinthe.getCoordG()[1],labyrinthe.getCoordG()[0]).passageSud(labyrinthe.plateau.getVal(labyrinthe.getCoordG()[1]-1,labyrinthe.getCoordG()[0])) :
+					labyrinthe.jouerTour("M",labyrinthe.getCoordG()[0]-1, labyrinthe.getCoordG()[1])
+				else : 
+					message = "La position n'est pas acessible"
 			elif event.type == KEYDOWN and event.key == K_LEFT :
-				labyrinthe.jouerTour("M",labyrinthe.getCoordG()[0], labyrinthe.getCoordG()[1]-1)
+				if labyrinthe.plateau.getVal(labyrinthe.getCoordG()[1],labyrinthe.getCoordG()[0]).passageOuest(labyrinthe.plateau.getVal(labyrinthe.getCoordG()[1],labyrinthe.getCoordG()[0]+1)) :
+					labyrinthe.jouerTour("M",labyrinthe.getCoordG()[0], labyrinthe.getCoordG()[1]-1)
+				else : 
+					message = "La position n'est pas acessible"
 			elif event.type == KEYDOWN and event.key == K_RIGHT :
-				labyrinthe.jouerTour("M",labyrinthe.getCoordG()[0], labyrinthe.getCoordG()[1]+1)
-			#~ elif : #click sur le bouton fin du tour
-				#~ changement = 0
-			afficheLaby(fenetre, labyrinthe)
+				if labyrinthe.plateau.getVal(labyrinthe.getCoordG()[1],labyrinthe.getCoordG()[0]).passageEst(labyrinthe.plateau.getVal(labyrinthe.getCoordG()[1]+1,labyrinthe.getCoordG()[0]-1)) :
+					labyrinthe.jouerTour("M",labyrinthe.getCoordG()[0], labyrinthe.getCoordG()[1]+1)
+				else : 
+					message = "La position n'est pas acessible"
+			elif event.type == MOUSEBUTTONDOWN and event.button == 1 and event.pos[1] > 975: #clique sur le bouton fin du tour
+				changement = 0
+		res = finTour(labyrinthe)
+		if res == 0 : # le joueur courant à gagné
+			continuer = 0
+			changement = 0
+		elif res == 1 : # le joueur courant à apporter un villageois au temple
+			message = "Le joueur %d viens de sacrifier un villageois" % (labyrinthe)
 		labyrinthe.changerJoueurCourant()
-
-#~ #Ouverture de la fenêtre Pygame
-#~ 
-#~ fenetre = pygame.display.set_mode((640, 480))
-#~ 
-#~ 
-#~ #Chargement et collage du fond
-#~ 
-#~ fond = pygame.image.load("background.jpg").convert()
-#~ 
-#~ fenetre.blit(fond, (0,0))
-#~ 
-#~ perso = pygame.image.load("perso.png").convert_alpha()
-#~ fenetre.blit(perso,(200,300))
-#~ 
-#~ #Rafraîchissement de l'écran
-#~ 
-#~ pygame.display.flip()
-#~ 
-#~ #BOUCLE INFINIE
-#~ 
-#~ continuer = 1
-#~ 
-#~ while continuer:
-	#~ for event in pygame.event.get():
-		#~ if event.type == QUIT:
-			#~ continuer = 0
-		#~ if event.type == KEYDOWN and event.key == K_UP :
-			#~ joueTour()
+		afficheLaby(fenetre, labyrinthe)
+		labyrinthe.changerJoueurCourant()
 
 
 # Création du labyrinthe
